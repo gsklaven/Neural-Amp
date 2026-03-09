70b160048a6d4497e6f735f76a815f094ef8b25d
giorgosskl <sklavenitisgeorge21@gmail.com>
2026-03-09 22:29:26 +0200

    fix plugin

--- juce_plugin/src/PluginProcessor.h
+++ juce_plugin/src/PluginProcessor.h
@@ -1,13 +1,24 @@
+#pragma once
+
+#include <JuceHeader.h>
 #include <RTNeural/RTNeural.h>
 
-class NeuralAmpAudioProcessor  : public juce::AudioProcessor
+class NeuralAmpAudioProcessor : public juce::AudioProcessor
 {
 public:
-    // ... διάφορες default συναρτήσεις του JUCE ...
+    NeuralAmpAudioProcessor();
+    ~NeuralAmpAudioProcessor() override;
+
+    // Εδώ το JUCE μας δίνει τον ήχο για επεξεργασία
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+    
+    // ... (παραλείπω μερικές ακόμα υποχρεωτικές συναρτήσεις του JUCE για συντομία) ...
 
 private:
-    // Εδώ φτιάχνουμε το "αντίγραφο" του PyTorch μοντέλου μας στη C++
     RTNeural::ModelT<float, 1, 1,
         RTNeural::LSTMLayerT<float, 1, 32>,
         RTNeural::DenseT<float, 32, 1>> model;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NeuralAmpAudioProcessor)
 };
